08d94386be36997b2c89fdf1e81ce96a6b01cb22
Femi Segun <femisegun@icloud.com>
2026-01-07 14:33:48 +0000

    tidying

--- utils.py
+++ utils.py
@@ -119,6 +119,7 @@ def get_desc_names():
     return descriptors
 
 def get_frag_names():
+    '''
     s = 'C'
     mol = Chem.MolFromSmiles(s)
     fragments = []
@@ -126,6 +127,23 @@ def get_frag_names():
         key, value = desc
         if key.startswith("fr_"):
             fragments.append(key)
+    '''
+    fragments = ["fr_Al_COO", "fr_Al_OH", "fr_Al_OH_noTert", "fr_ArN", "fr_Ar_COO",
+                     "fr_Ar_N", "fr_Ar_NH", "fr_Ar_OH", "fr_COO", "fr_COO2", "fr_C_O",
+                     "fr_C_O_noCOO", "fr_C_S", "fr_HOCCN", "fr_Imine", "fr_NH0", "fr_NH1",
+                     "fr_NH2", "fr_N_O", "fr_Ndealkylation1", "fr_Ndealkylation2", "fr_Nhpyrrole",
+                     "fr_SH", "fr_aldehyde", "fr_alkyl_carbamate", "fr_alkyl_halide", "fr_allylic_oxid",
+                     "fr_amide", "fr_amidine", "fr_aniline", "fr_aryl_methyl", "fr_azide", "fr_azo",
+                     "fr_barbitur", "fr_benzene", "fr_benzodiazepine", "fr_bicyclic", "fr_diazo",
+                     "fr_dihydropyridine", "fr_epoxide", "fr_ester", "fr_ether", "fr_furan", "fr_guanido",
+                     "fr_halogen", "fr_hdrzine", "fr_hdrzone", "fr_imidazole", "fr_imide", "fr_isocyan",
+                     "fr_isothiocyan", "fr_ketone", "fr_ketone_Topliss", "fr_lactam", "fr_lactone", "fr_methoxy",
+                     "fr_morpholine", "fr_nitrile", "fr_nitro", "fr_nitro_arom", "fr_nitro_arom_nonortho",
+                     "fr_nitroso", "fr_oxazole", "fr_oxime", "fr_para_hydroxylation", "fr_phenol",
+                     "fr_phenol_noOrthoHbond", "fr_phos_acid", "fr_phos_ester", "fr_piperdine", "fr_piperzin"
+                     "", "fr_priamide", "fr_prisulfonamd", "fr_pyridine", "fr_quatN", "fr_sulfide", "fr_sulfonamd",
+                     "fr_sulfone", "fr_term_acetylene", "fr_tetrazole", "fr_thiazole", "fr_thiocyan",
+                     "fr_thiophene", "fr_unbrch_alkane", "fr_urea"]
 
     return fragments
 
@@ -444,7 +462,7 @@ def scatter_plot(y_test, pred, save_to=None, title: str = None):
     plt.show()
 
 def bar_plot(mers_results: pd.DataFrame, sars_results: pd.DataFrame, save_to: str = None):
-    fig, axes = plt.subplots(2, 2, figsize = (10, 6), constrained_layout = True)
+    fig, axes = plt.subplots(2, 2, figsize = (10, 6), constrained_layout = True, sharey = "row")
 
     palette = ["#7920d3bb",'#e69f00bb']
 
@@ -468,6 +486,12 @@ def bar_plot(mers_results: pd.DataFrame, sars_results: pd.DataFrame, save_to: st
     axes[0, 1].set_title("pIC50 SARS: CV R²")
     axes[1, 0].set_title("pIC50 MERS: CV MAE")
     axes[1, 1].set_title("pIC50 SARS: CV MAE")
+
+    #axes[0, 0].set_xlabel("Scheme")
+    axes[0, 0].set_ylabel("R²")
+
+    #axes[1, 0].set_xlabel("Scheme")
+    axes[1, 0].set_ylabel("MAE")
 
     for ax in axes.flat:
         ax.set_xlabel("")
